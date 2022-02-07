49ca68c77a7f1db38c56d876f3c67b0059dce8b3
laurati <lauraecomarquez@gmail.com>
2022-02-07 16:00:31 -0300

    block_id e block_parent  ok

--- models/models.go
+++ models/models.go
@@ -8,10 +8,10 @@ import (
 )
 
 type BQData struct {
-	ClientBQ_id int
-	Block_id    int
-	//Block_parent int
-	Block_name string
+	ClientBQ_id  int
+	Block_id     int
+	Block_parent int
+	Block_name   string
 }
 
 type Client struct {
@@ -46,6 +46,7 @@ type Areas_2 struct {
 
 type Branch struct {
 	Client_id    int
+	Branch_id    int
 	Branch_name  string
 	Area_farm_id int
 }
@@ -68,6 +69,32 @@ type Farm_2 struct {
 	Block_id     int
 	Block_parent int
 	Farm_name    string
+}
+
+var FarmList []int
+
+func VerifyFarmID(farmID int) bool {
+	var status bool = false
+	for _, x := range FarmList {
+		if x == farmID {
+			status = true
+			break
+		}
+	}
+	return status
+}
+
+var BranchList []int
+
+func VerifyBranchID(branchID int) bool {
+	var status bool = false
+	for _, x := range BranchList {
+		if x == branchID {
+			status = true
+			break
+		}
+	}
+	return status
 }
 
 func ReadPg() {
@@ -133,7 +160,7 @@ func ReadPg() {
 		client_2 = Client_2{client.Client_id, block_id, 0, client.Client_name}
 		clientPg_2 = append(clientPg_2, client_2)
 
-		bqData = BQData{client.Client_id, block_id, client.Client_name}
+		bqData = BQData{client.Client_id, block_id, 0, client.Client_name}
 		bqDataArr = append(bqDataArr, bqData)
 
 		block_id_cli = block_id
@@ -178,20 +205,9 @@ func ReadPg() {
 					log.Println("Error:", err.Error())
 				}
 
-				// areasPg = append(areasPg, areas)
-
-				// areas_2 = Areas_2{areas.Client_id, block_id, areas.Areas_name}
-
-				// areas_2_Pg = append(areas_2_Pg, areas_2)
-
-				// bqData = BQData{areas.Client_id, block_id, areas.Areas_name}
-				// bqDataArr = append(bqDataArr, bqData)
-
-				// block_id++
-
 				//branch
 				queryBranch := fmt.Sprintf(
-					"select " + strconv.Itoa(mapping.Client_id) + " as Client_id , " + "b.name as branch_name, b.area_farm_id " +
+					"select " + strconv.Itoa(mapping.Client_id) + " as Client_id , " + "b.area_branch_id as branch_id, b.name as branch_name, b.area_farm_id " +
 						"from area_branch b " +
 						"where b.area_branch_id = " + strconv.Itoa(areas.Areas_branch_id))
 
@@ -201,25 +217,14 @@ func ReadPg() {
 				}
 				for rows.Next() {
 
-					err := rows.Scan(&branch.Client_id, &branch.Branch_name, &branch.Area_farm_id)
+					err := rows.Scan(&branch.Client_id, &branch.Branch_id, &branch.Branch_name, &branch.Area_farm_id)
 					if err != nil {
 						log.Println("Error:", err.Error())
 					}
 
-					// branchPg = append(branchPg, branch)
-
-					// branch_2 = Branch_2{branch.Client_id, block_id, block_id_farm, branch.Branch_name}
-
-					// branch_2_Pg = append(branch_2_Pg, branch_2)
-
-					// bqData = BQData{branch.Client_id, block_id, branch.Branch_name}
-					// bqDataArr = append(bqDataArr, bqData)
-
-					// block_id++
-
 					//farm
 					queryFarm := fmt.Sprintf(
-						"select " + strconv.Itoa(mapping.Client_id) + " as Client_id , f.area_farm_id, " + "f.name as farm_name " +
+						"select distinct " + strconv.Itoa(mapping.Client_id) + " as Client_id , f.area_farm_id, " + "f.name as farm_name " +
 							"from area_farm f " +
 							"where f.area_farm_id = " + strconv.Itoa(branch.Area_farm_id))
 
@@ -235,18 +240,24 @@ func ReadPg() {
 							log.Println("Error:", err.Error())
 						}
 
-						//if para validar se farm.Farm_id ja existe no array
-
 						farmPg = append(farmPg, farm)
 
 						farm_2 = Farm_2{farm.Client_id, block_id, block_id_cli, farm.Farm_name}
 
-						farmPg_2 = append(farmPg_2, farm_2)
+						bqData = BQData{farm.Client_id, block_id, block_id_cli, farm.Farm_name}
 
-						block_id_farm = block_id
+						//if para validar se farm.Farm_id ja existe no array
+						if !VerifyFarmID(farm.Farm_id) { //Element is not present in the slice
+							FarmList = append(FarmList, farm.Farm_id)
+							farmPg_2 = append(farmPg_2, farm_2)
+							bqDataArr = append(bqDataArr, bqData)
+							block_id_farm = block_id
 
-						bqData = BQData{farm.Client_id, block_id, farm.Farm_name}
-						bqDataArr = append(bqDataArr, bqData)
+						}
+
+						// bqData = BQData{farm.Client_id, block_id, block_id_cli, farm.Farm_name}
+
+						// bqDataArr = append(bqDataArr, bqData)
 
 						block_id++
 
@@ -255,12 +266,17 @@ func ReadPg() {
 
 					branch_2 = Branch_2{branch.Client_id, block_id, block_id_farm, branch.Branch_name}
 
-					branch_2_Pg = append(branch_2_Pg, branch_2)
+					bqData = BQData{branch.Client_id, block_id, block_id_farm, branch.Branch_name}
 
-					bqData = BQData{branch.Client_id, block_id, branch.Branch_name}
-					bqDataArr = append(bqDataArr, bqData)
+					if !VerifyBranchID(branch.Branch_id) { //Element is not present in the slice
+						BranchList = append(BranchList, branch.Branch_id)
+						branch_2_Pg = append(branch_2_Pg, branch_2)
+						bqDataArr = append(bqDataArr, bqData)
+						block_id_branch = block_id
+					}
 
-					block_id_branch = block_id
+					// bqData = BQData{branch.Client_id, block_id, branch.Branch_name}
+					// bqDataArr = append(bqDataArr, bqData)
 
 					block_id++
 
@@ -271,16 +287,16 @@ func ReadPg() {
 
 				areas_2_Pg = append(areas_2_Pg, areas_2)
 
-				bqData = BQData{areas.Client_id, block_id, areas.Areas_name}
+				bqData = BQData{areas.Client_id, block_id, block_id_branch, areas.Areas_name}
 				bqDataArr = append(bqDataArr, bqData)
 
-				block_id++
-
 			}
+			block_id++
 
 		}
 
 	}
+
 	fmt.Println("cli", clientPg_2)
 	fmt.Println("=========")
 	fmt.Println("map", mappingPg)
@@ -291,6 +307,6 @@ func ReadPg() {
 	fmt.Println("=========")
 	fmt.Println("farm", farmPg_2)
 	fmt.Println("=========")
-	// fmt.Println("bqdata", bqDataArr)
+	fmt.Println("bqdata", bqDataArr)
 
 }
